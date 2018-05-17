0942e9b84f1a04a94f118628e41ca9d25e664217
David Miller <hawky454@gmail.com>
2018-05-17 12:08:54 -0500

    My first functional JS application

--- index.js
+++ index.js
@@ -1,5 +1,49 @@
 (function() {
   'use strict';
 
-  // YOUR CODE HERE
+let redLight =  document.getElementById('stopLight');
+let yellowLight = document.getElementById('slowLight');
+let greenLight = document.getElementById('goLight');
+
+let controls = document.getElementById('controls');
+let stopButton = document.getElementById('stopButton');
+let slowButton = document.getElementById('slowButton');
+let goButton = document.getElementById('goButton');
+
+let stopLightOn = false;
+let slowLightOn = false;
+let goButtonOn = false;
+
+
+stopButton.addEventListener('click', function() {
+  // When you first select this button, turn the STOP LIGHT red.
+  // When you select this button again, turn the STOP LIGHT black.
+  if(stopLightOn === true) {
+    redLight.style.backgroundColor = 'black';
+    stopLightOn = false;
+  } else {
+      redLight.style.backgroundColor = 'red';
+      stopLightOn = true;
+    }
+  });
+
+  slowButton.addEventListener('click', function() {
+    if(slowLightOn === true) {
+      yellowLight.style.backgroundColor = 'black';
+      slowLightOn = false;
+    } else {
+      yellowLight.style.backgroundColor = 'orange';
+      slowLightOn = true;
+    }
+  });
+
+goButton.addEventListener('click', function() {
+  if(goButtonOn === true) {
+    greenLight.style.backgroundColor = 'black';
+    goButtonOn = false;
+  } else {
+    greenLight.style.backgroundColor = 'green';
+    goButtonOn = true;
+  }
+});
 })();
